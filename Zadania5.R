93be6b54fb4fc18db34ff0e5f106466a81956325
Agata <37178984+agatazbikowska@users.noreply.github.com>
2018-06-11 15:40:18 +0200

    Update Zadania5.R

--- Zadania5.R
+++ Zadania5.R
@@ -8,11 +8,11 @@ library(readr)
 movies <- read_csv("movies.csv")
 View(movies)
 
-#zadanie5-2
+#zadanie 5-2
 filter(movies, Comedy, year == 2005)
 
 
-#zadanie5-3
+#zadanie 5-3
 select(movies, title, year, budget) %>% 
   arrange(desc(budget))
 
@@ -21,3 +21,12 @@ select(movies, title, year, budget) %>%
 filter(movies, Animation, year == 1990) %>% 
   arrange(desc(rating))
 
+
+#zadanie 5-5
+dramaty <- filter(movies, Drama == 1)
+arrange(dramaty, desc(length))
+
+
+#zadanie 5-6
+mpaa_rating <- group_by(movies, mpaa) %>%
+summarise (srednia = mean(rating), odchylenie = mad(rating))
